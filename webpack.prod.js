f5ab29ebd3452113db425ad2533d9f8c5b63f328
Isra bazzar <isra2.badaha@gmail.com>
2022-09-20 07:16:00 +0300

    Fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,51 +1,60 @@
 const path = require('path')
 const webpack = require("webpack")
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-const WorkboxPlugin = require('workbox-webpack-plugin')
+const {GenerateSW} = require('workbox-webpack-plugin');
+
+// Configuration for service worker - https://www.npmjs.com/package/service-worker-plugin
+const rootConfig = {
+    cache: {
+        offline: true,
+        precache: ['\\.js'],
+        strategy: [{
+            type: 'prefer-cache',
+            matches: ['\\.js']
+        }]
+    }
+};
+
+/***************************************** */
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
-    devServer: {port: 3000},
+    devServer: {
+        port: 3000
+    },
+    output: {
+        publicPath: './js/',
+    },
     module: {
-        rules: [
-                    {
+        rules: [{
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
-                    },
-                    {
-        test: /.scss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-}   ,
-{
-            test: /\.(png|jpg|gif)$/,
-            use: [
-                {
+            },
+            {
+                test: /.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
                     loader: 'file-loader',
                     options: {}
-                }
-            ]
-        }            
-                    
-            ]
-    },
-    
-    plugins: [
-    new HtmlWebPackPlugin({
-        template: "./src/client/views/index.html",
-        filename: "./index.html",
-    }),
-    new WorkboxPlugin.GenerateSW(),
+                }]
+            }
 
-     new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: false,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
+        ]
+    },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
         }),
-]
+
+        // workbox plugin setup
+        // https://developer.chrome.com/docs/workbox/modules/workbox-webpack-plugin/
+        new GenerateSW(),
+    ]
 }
